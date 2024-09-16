45bd6f6803fa5f14f1ab5af32243112c45a77b9d
anton0998 <anton.heiden@hotmail.com>
2024-09-16 11:00:07 +0200

    added delete func

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import ToDo from './components/ToDo';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="my-5 mx-auto">
+      <h1 className='text-center'>
+        To-do list
+      </h1>
+      <ToDo />
     </div>
   );
 }
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,74 @@
+import { Button, FloatingLabel, Form, ListGroup, button } from "react-bootstrap/";
+import { useState } from "react";
+
+export default function ToDo() {
+  const [task, setTask] = useState("");
+  const [newTodo, setNewTodo] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setNewTodo((prev) => [
+      ...prev,
+      {
+        id: crypto.randomUUID(), // Generer et unikt id for hver opgave
+        title: task,
+        completed: false, 
+      },
+    ]);
+
+    if (setTask === '') {
+      return null
+    }
+
+    setTask("");
+  };
+
+  const handleComplete = (id) => {
+    setNewTodo((prevTodos) =>
+      prevTodos.map((todoItem) =>
+        todoItem.id === id ? { ...todoItem, completed: !todoItem.completed } : todoItem
+      )
+    );
+  };
+
+  const handleDelete = () => {
+    setNewTodo(prevTodo => prevTodo.filter((todoItem) => !todoItem.completed))
+  }
+
+  return (
+    <div className="col-6 m-auto my-3">
+      <form onSubmit={handleSubmit}>
+        <FloatingLabel label="Enter task:" className="d-flex gap-2 my-5">
+          <Form.Control
+            type="text"
+            placeholder="Enter task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+        </FloatingLabel>
+        <Button onClick={handleDelete} className="mb-2" size='sm' variant="outline-danger">Slet færdige</Button>
+        <ListGroup>
+          {newTodo.map((todoItem) => (
+            <ListGroup.Item
+              className='d-flex gap-2 align-items-center'
+              key={todoItem.id}
+              style={{
+                textDecoration: todoItem.completed ? "line-through" : "none",
+                opacity: todoItem.completed ? 0.5 : 1, 
+                cursor: 'pointer',
+              }}
+              onClick={() => handleComplete(todoItem.id)}
+            >
+              <Form.Check
+                type="checkbox"
+                checked={todoItem.completed} 
+              />
+              {todoItem.title}
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </form>
+    </div>
+  );
+}
