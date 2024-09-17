d006a10ea940a88a86c9db8bef0a86bfa1e3530e
anton0998 <anton.heiden@hotmail.com>
2024-09-17 14:53:12 +0200

    added priorities

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,36 +1,40 @@
-import { Button, FloatingLabel, Form, ListGroup, } from "react-bootstrap/";
+import { Button, FloatingLabel, Form, ListGroup, Badge } from "react-bootstrap/";
 import { useState } from "react";
 import ShowProgress from './ShowProgress'
+// import HandlePriority from "./HandlePriority"; 
 
 export default function TodoList() {
   const initialTodos = [
-    {id: crypto.randomUUID(), title: 'Vaske tøj', completed: false },
-    {id: crypto.randomUUID(), title: 'Skifte sengetøj', completed: false },
-    {id: crypto.randomUUID(), title: 'Handle ind', completed: false },
-    {id: crypto.randomUUID(), title: 'Lave mad', completed: false },
-    {id: crypto.randomUUID(), title: 'Træne', completed: false },
+    {id: crypto.randomUUID(), title: 'Vaske tøj', completed: false, priority: 'high'},
+    {id: crypto.randomUUID(), title: 'Skifte sengetøj', completed: false, priority: 'low'},
+    {id: crypto.randomUUID(), title: 'Handle ind', completed: false, priority: 'medium'},
+    {id: crypto.randomUUID(), title: 'Lave mad', completed: false, priority: 'medium'},
+    {id: crypto.randomUUID(), title: 'Træne', completed: false, priority: 'high'},
   ]
 
   const [task, setTask] = useState("");
   const [newTodo, setNewTodo] = useState(initialTodos);
+  const [priority, setPriority] = useState('low')
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (task.trim() === '') {
+      return null
+    };
 
     setNewTodo((prev) => [
       ...prev,
       {
         id: crypto.randomUUID(), // Generer et unikt id for hver opgave
         title: task,
-        completed: false, 
+        completed: false,
+        priority: priority, 
       },
     ]);
 
-    if (setTask === '') {
-      return null
-    }
-
-    setTask("");
+    setTask('');
+    setPriority('');
   };
 
   const handleComplete = (id) => {
@@ -56,22 +60,39 @@ export default function TodoList() {
     setNewTodo(prevTodo => prevTodo.filter((todoItem) => !todoItem.completed))
   }
 
-  // const showProgress = () => {
-  //   const getProgress = newTodo.filter((todoItem) => todoItem.completed);
-  //   return <ProgressBar now={getProgress} />
-  // }
+  const getBadgeVariant = (priority) => {
+    switch (priority) {
+      case 'high':
+        return 'danger';
+      case 'medium':
+        return 'warning';
+      case 'low':
+        return 'success';
+      default:
+        return 'secondary';
+    }
+  };
 
   return (
-    <div className="col-6 m-auto">
+    <div className="m-auto col-6">
       <form onSubmit={handleSubmit}>
-        <FloatingLabel label="Enter task:" className="d-flex gap-2 mt-2 mb-6">
-          <Form.Control
-            type="text"
-            placeholder="Enter task"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-          />
-        </FloatingLabel>
+        <div>
+          <FloatingLabel label="Enter task:" className="d-flex gap-2 mb-6">
+            <Form.Control
+              style={{ flex: '3' }}
+              type="text"
+              placeholder="Enter task"
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
+            />
+            <Form.Select onSelect={e => setPriority(e.target.value)} style={{ flex: '1' }}>
+              <option className='align-items-center' >Vælg prioritet</option>
+              <option value="low">Lav</option>
+              <option value="medium">Mellem</option>
+              <option value="high">Høj</option>
+            </Form.Select>
+          </FloatingLabel>
+        </div>
         <dir className='my-4'>
           <ShowProgress todos={newTodo} />
         </dir>
@@ -95,6 +116,9 @@ export default function TodoList() {
                 type="checkbox"
                 checked={todoItem.completed} 
               />
+              <Badge bg={getBadgeVariant(todoItem.priority)}>
+                  {todoItem.priority}
+              </Badge>
               {todoItem.title}
             </ListGroup.Item>
           ))}
